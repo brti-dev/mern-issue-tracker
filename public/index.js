4af15fc6329e3ece22dc95d9d2f653272795ac88
dr-spaceman <mat.berti@gmail.com>
2020-06-18 00:26:11 +0800

    Issue tracker React basic structure

--- public/index.js
+++ public/index.js
@@ -1,25 +1,124 @@
 "use strict";
 
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+var initialIssues = [{
+  id: 1,
+  status: 'New',
+  owner: 'Ravan',
+  effort: 5,
+  created: new Date('2019-01-15'),
+  due: undefined,
+  title: 'Error in console when clicking "Add"'
+}, {
+  id: 2,
+  status: 'Assigned',
+  owner: 'Eddie',
+  effort: 14,
+  created: new Date('2019-01-16'),
+  due: new Date('2019-02-16'),
+  title: 'Missing bottom border panel'
+}];
+
+var sampleIssue = _objectSpread(_objectSpread({}, initialIssues[0]), {}, {
+  status: 'New',
+  owner: 'Pieta'
+});
+
+function loadData() {
+  return new Promise(function (resolve, reject) {
+    return setTimeout(function () {
+      return resolve({
+        data: initialIssues
+      });
+    }, 1000);
+  });
+}
+
 function IssueFilter() {
   return /*#__PURE__*/React.createElement("div", null, "foo");
 }
 
 function IssueTable() {
-  return /*#__PURE__*/React.createElement("table", null, /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", null, "ID"), /*#__PURE__*/React.createElement("th", null, "Title"))), /*#__PURE__*/React.createElement("tbody", null, /*#__PURE__*/React.createElement(IssueRow, {
-    issue_id: 1,
-    issue_title: "Error foo"
-  }), /*#__PURE__*/React.createElement(IssueRow, {
-    issue_id: 2,
-    issue_title: "Error bar"
-  })));
+  var _React$useState = React.useState([]),
+      _React$useState2 = _slicedToArray(_React$useState, 2),
+      issues = _React$useState2[0],
+      setIssues = _React$useState2[1];
+
+  React.useEffect(function () {
+    loadData().then(function (result) {
+      setIssues(result.data);
+    });
+  }, []);
+
+  var createIssue = function createIssue(issue) {
+    issue.id = issues.length + 1;
+    issue.created = new Date();
+    var newIssueList = issues.slice();
+    newIssueList.push(issue);
+    setIssues(newIssueList);
+  };
+
+  var issueRows = issues.map(function (issue) {
+    return /*#__PURE__*/React.createElement(IssueRow, {
+      key: issue.id,
+      issue: issue
+    });
+  });
+  var formRef = React.useRef();
+
+  var handleSubmit = function handleSubmit(e) {
+    e.preventDefault();
+    var form = formRef.current;
+    var issue = {
+      owner: form.owner.value,
+      title: form.title.value,
+      status: 'New'
+    };
+    createIssue(issue);
+  };
+
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("table", {
+    border: "1"
+  }, /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", null, "ID"), /*#__PURE__*/React.createElement("th", null, "Status"), /*#__PURE__*/React.createElement("th", null, "Owner"), /*#__PURE__*/React.createElement("th", null, "Created"), /*#__PURE__*/React.createElement("th", null, "Effort"), /*#__PURE__*/React.createElement("th", null, "Due Date"), /*#__PURE__*/React.createElement("th", null, "Title"))), /*#__PURE__*/React.createElement("tbody", null, issueRows)), /*#__PURE__*/React.createElement("form", {
+    ref: formRef,
+    onSubmit: handleSubmit
+  }, /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    name: "owner",
+    placeholder: "Owner"
+  }), /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    name: "title",
+    placeholder: "Title"
+  }), /*#__PURE__*/React.createElement("button", {
+    type: "submit"
+  }, "Add")));
 }
 
-function IssueAdd() {
-  return /*#__PURE__*/React.createElement("div", null, "foo");
+function IssueRow(_ref) {
+  var issue = _ref.issue;
+  return /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", null, issue.id), /*#__PURE__*/React.createElement("td", null, issue.status), /*#__PURE__*/React.createElement("td", null, issue.owner), /*#__PURE__*/React.createElement("td", null, issue.created.toDateString()), /*#__PURE__*/React.createElement("td", null, issue.effort), /*#__PURE__*/React.createElement("td", null, issue.due && issue.due.toDateString()), /*#__PURE__*/React.createElement("td", null, issue.title));
 }
 
 function IssueList() {
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Issue Tracker"), /*#__PURE__*/React.createElement(IssueFilter, null), /*#__PURE__*/React.createElement(IssueTable, null), /*#__PURE__*/React.createElement(IssueAdd, null));
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Issue Tracker"), /*#__PURE__*/React.createElement(IssueFilter, null), /*#__PURE__*/React.createElement(IssueTable, null));
 }
 
 var element = /*#__PURE__*/React.createElement(IssueList, null);
--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,112 @@
+const initialIssues = [
+    {
+        id: 1,
+        status: 'New',
+        owner: 'Ravan',
+        effort: 5,
+        created: new Date('2019-01-15'),
+        due: undefined,
+        title: 'Error in console when clicking "Add"',
+    },
+    {
+        id: 2,
+        status: 'Assigned',
+        owner: 'Eddie',
+        effort: 14,
+        created: new Date('2019-01-16'),
+        due: new Date('2019-02-16'),
+        title: 'Missing bottom border panel',
+    }
+]
+
+const sampleIssue = {...initialIssues[0], status: 'New', owner: 'Pieta'}
+
+function loadData() {
+    return new Promise((resolve, reject) =>
+        setTimeout(
+            () => resolve({ data: initialIssues }),
+            1000
+        )
+    )
+}
+
 function IssueFilter() {
     return <div>foo</div>
 }
 
 function IssueTable() {
+    const [issues, setIssues] = React.useState([]);
+
+    React.useEffect(() => {
+        loadData().then(result => {
+            setIssues(result.data)
+        })
+    }, [])
+
+    const createIssue = (issue) => {
+        issue.id = issues.length + 1;
+        issue.created = new Date();
+        const newIssueList = issues.slice();
+        newIssueList.push(issue);
+        setIssues(newIssueList);
+    }
+
+    const issueRows = issues.map(issue => 
+        <IssueRow key={issue.id} issue={issue} />
+    )
+
+    const formRef = React.useRef()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const form = formRef.current
+        const issue = {
+            owner: form.owner.value,
+            title: form.title.value,
+            status: 'New',
+        }
+        createIssue(issue)
+    }
+
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Title</th>
-                </tr>
-            </thead>
-            <tbody>
-                <IssueRow issue_id={1} issue_title="Error foo" />
-                <IssueRow issue_id={2} issue_title="Error bar" />
-            </tbody>
-        </table>
+        <>
+            <table border="1">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Status</th>
+                        <th>Owner</th>
+                        <th>Created</th>
+                        <th>Effort</th>
+                        <th>Due Date</th>
+                        <th>Title</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {issueRows}
+                </tbody>
+            </table>
+            <form ref={formRef} onSubmit={handleSubmit}>
+                <input type="text" name="owner" placeholder="Owner" />
+                <input type="text" name="title" placeholder="Title" />
+                <button type="submit">Add</button>
+            </form>
+        </>
     )
 }
 
-function IssueAdd() {
-    return <div>foo</div>
+function IssueRow({issue}) {
+    return (
+        <tr>
+            <td>{issue.id}</td>
+            <td>{issue.status}</td>
+            <td>{issue.owner}</td>
+            <td>{issue.created.toDateString()}</td>
+            <td>{issue.effort}</td>
+            <td>{issue.due && issue.due.toDateString()}</td>
+            <td>{issue.title}</td>
+        </tr>
+    );
 }
 
 function IssueList() {
@@ -29,7 +115,6 @@ function IssueList() {
             <h1>Issue Tracker</h1>
             <IssueFilter />
             <IssueTable />
-            <IssueAdd />
         </>
     )
 }
