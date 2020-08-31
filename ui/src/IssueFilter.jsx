d731bb27ccad18ae1ee9c46a157652b996fe8915
Mathew Berti <mathewberti@Mathews-MacBook-Air.local>
2020-08-31 17:13:55 +0800

    Edit Form:

--- ui/src/IssueFilter.jsx
+++ ui/src/IssueFilter.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import URLSearchParams from '@ungap/url-search-params';
 import { withRouter } from 'react-router-dom';
 
+/* eslint "no-alert": "off" */
+
 /**
  * Navigation and functions for filtering IssueTable.
  * Component not a <Route> so need to pass through withRouter function to get
@@ -15,32 +17,63 @@ const IssueFilter = withRouter((props) => {
     // Read location qs to determine selectform value
     const { location: { search } } = props;
     const queryParams = new URLSearchParams(search);
-    const [status, setStatus] = React.useState(queryParams.get('status') || '');
 
-    // Change state:status
-    function onChangeStatus(element) {
-        setStatus(element.target.value);
+    const initialState = {
+        status: queryParams.get('status') || '',
+        effortMin: queryParams.get('effortMin') || '',
+        effortMax: queryParams.get('effortMax') || '',
+    };
+    const [state, setState] = React.useState(initialState);
+
+    // On form change, update state
+    function handleChange(element) {
+        const { name, value } = element.target;
+
+        // Validate input
+        if (name === 'effortMin' || name === 'effortMax') {
+            if (!value.match(/^\d*$/)) {
+                alert('Input field `effort` can only contain numbers');
+                return false;
+            }
+        }
+
+        setState({
+            ...state,
+            [name]: value,
+        });
+
+        return true;
     }
 
     // Change URL and apply filter based on state:status
     function applyFilter() {
         const { history } = props;
+
+        const newQuery = new URLSearchParams();
+        if (state.status) newQuery.set('status', state.status);
+        if (state.effortMin) newQuery.set('effortMin', state.effortMin);
+        if (state.effortMax) newQuery.set('effortMax', state.effortMax);
+
         history.push({
             pathname: '/issues/',
-            search: status ? `status=${status}` : '',
+            search: newQuery.toString(),
         });
     }
 
     return (
         <div>
             Status:&nbsp;
-            <select value={status} onChange={onChangeStatus}>
+            <select name="status" value={state.status} onChange={handleChange}>
                 <option value="">All</option>
                 <option value="New">New</option>
                 <option value="Assigned">Assigned</option>
                 <option value="Fixed">Fixed</option>
                 <option value="Closed">Closed</option>
             </select>
+            Effort between:
+            <input type="number" size={5} name="effortMin" value={state.effortMin} onChange={handleChange} />
+            {' - '}
+            <input type="number" size={5} name="effortMax" value={state.effortMax} onChange={handleChange} />
             <button type="button" onClick={applyFilter}>Apply</button>
         </div>
     );
