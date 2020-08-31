eb93687db7cb319380cceb53b886a5f8e78972dd
Mathew Berti <mathewberti@Mathews-MacBook-Air.local>
2020-08-31 11:40:01 +0800

    Controlled Components: use React state to control variables and components in forms

--- ui/src/IssueFilter.jsx
+++ ui/src/IssueFilter.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import URLSearchParams from '@ungap/url-search-params';
 import { withRouter } from 'react-router-dom';
 
 /**
@@ -8,11 +9,21 @@ import { withRouter } from 'react-router-dom';
  *
  * @param {Object} props Props supplied by React Router via withRouter method
  */
-function IssueFilter(props) {
+const IssueFilter = withRouter((props) => {
     console.log('IssueFilter compontent', props);
 
+    // Read location qs to determine selectform value
+    const { location: { search } } = props;
+    const queryParams = new URLSearchParams(search);
+    const [status, setStatus] = React.useState(queryParams.get('status') || '');
+
+    // Change state:status
     function onChangeStatus(element) {
-        const status = element.target.value;
+        setStatus(element.target.value);
+    }
+
+    // Change URL and apply filter based on state:status
+    function applyFilter() {
         const { history } = props;
         history.push({
             pathname: '/issues/',
@@ -23,15 +34,16 @@ function IssueFilter(props) {
     return (
         <div>
             Status:&nbsp;
-            <select onChange={onChangeStatus}>
+            <select value={status} onChange={onChangeStatus}>
                 <option value="">All</option>
                 <option value="New">New</option>
                 <option value="Assigned">Assigned</option>
                 <option value="Fixed">Fixed</option>
                 <option value="Closed">Closed</option>
             </select>
+            <button type="button" onClick={applyFilter}>Apply</button>
         </div>
     );
-}
+});
 
-export default withRouter(IssueFilter);
+export default IssueFilter;
