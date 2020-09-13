37f86e13969cad1c2381d89f36ed62e5399662af
Mathew Berti <mathewberti@Mathews-MacBook-Air.local>
2020-09-13 19:26:29 +0800

    Basic nav bar (needs work!)

--- ui/src/Page.jsx
+++ ui/src/Page.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Badge from '@material-ui/core/Badge';
+import Chip from '@material-ui/core/Chip';
+
 import Contents from './Contents.jsx';
 
 function NavBar() {
     return (
-        <nav>
-            <NavLink exact to="/">Home</NavLink>
-            <span>|</span>
-            <NavLink to="/issues">Issue List</NavLink>
-            <span>|</span>
-            <NavLink to="/report">Report</NavLink>
-        </nav>
+        <AppBar position="static">
+            <Toolbar>
+                <Typography variant="h5">
+                    Issue Tracker{' '}
+                    <Chip label="MERN!" color="secondary" size="small" />
+                </Typography>
+                <Typography>
+                    <NavLink to="/issues">Issue List</NavLink>{' '}
+                    <NavLink to="/report">Report</NavLink>
+                </Typography>
+            </Toolbar>
+        </AppBar>
     );
 }
 
